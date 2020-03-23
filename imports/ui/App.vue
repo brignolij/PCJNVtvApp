d086c4c47b0b2bba9c8924b46ff34cf58de83aea
Jeffrey Brignoli <>
2020-03-23 18:59:45 +0100

    initial commit

---
+++ imports/ui/App.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Todo List</h1>
+      <form class="new-task" @submit.prevent="handleSubmit">
+        <input type="text" placeholder="Type to add new tasks" v-model="newTask" />
+      </form>
+    </header>
+
+    <ul>
+      <Task v-for="task in tasks" v-bind:key="task.index" v-bind:task="task" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Task from "./Task.vue";
+import { Tasks } from "./../api/tasks.js";
+export default {
+  components: {
+    Task
+  },
+
+  data() {
+    return {
+      newTask: ""
+    };
+  },
+
+  methods: {
+    handleSubmit(event) {
+      Tasks.insert({
+        text: this.newTask,
+
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+
+      this.newTask = "";
+    }
+  },
+  meteor: {
+    tasks() {
+      return Tasks.find({}).fetch();
+    }
+  }
+};
+</script>
+
---
+++ imports/ui/Task.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <li>{{ this.task.text }}</li>
+</template>
+
+ 
+
+<script>
+export default {
+  props: ["task"],
+
+  data() {
+    return {};
+  }
+};
+</script>
+
